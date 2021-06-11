7d627ec75af608b412e119e858743761420ea7fc
samq-ws <sam.quakenbush@whitesourcesoftware.com>
2021-06-11 08:57:00 -0600

    searchdir + releasedir

--- xModuleAnalyzer-NET.sh
+++ xModuleAnalyzer-NET.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # For use with linux actions in https://github.com/whitesource-ft/ws-examples/tree/main/Prioritize/DotNet/Multi-Module
-
-for csproject in  $(find ./src -type f \( -wholename "*.csproj" ! -wholename "*build*" ! -wholename "*test*" ! -wholename "*host*" ! -wholename "*migration*" \))
+# Modify SEARCHDIR & RELEASEDIR before running
+SEARCHDIR=$(PWD)
+RELEASEDIR=*bin*
+for csproject in  $(find $SEARCHDIR -type f \( -wholename "*.csproj" ! -wholename "*build*" ! -wholename "*test*" ! -wholename "*host*" ! -wholename "*migration*" \))
 
 do
 echo "Found" $csproject 
 CSPROJ=$(basename $csproject .csproj)
 
-find ./ -type f \( -wholename "*/bin/Release/*/$CSPROJ.dll" ! -wholename "*build*" ! -wholename "*test*" ! -wholename "*host*" ! -wholename "*migration*" \) -print >> multi-module.txt
+find ./ -type f \( -wholename "$RELEASEDIR$CSPROJ.dll" ! -wholename "*build*" ! -wholename "*test*" ! -wholename "*host*" ! -wholename "*migration*" \) -print >> multi-module.txt
 
 done
 
@@ -18,7 +20,7 @@ for DLL in $dlls;
 
 do 
 echo "appPath:" $DLL
-DIR=$(echo $DLL | awk -F/bin '{print $1}')
+DIR=$(echo $DLL | awk -F $RELEASEDIR '{print $1}')
 echo "directory:" $DIR
 PROJECT=$(basename $DLL .dll)
 echo "PROJECT:" $PROJECT
